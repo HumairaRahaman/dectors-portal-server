0cd5708ec43b7cc196ce95aa3d70172d963ce603
Humaira Rahaman <humairamitul@gmail.com>
2022-05-15 03:15:55 +0600

    jwt token verify account and make user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
@@ -14,12 +15,52 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+//jwt fun for appointment
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized access'});
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 
 async function run(){
 try{
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services')
     const bookingCollection = client.db('doctors_portal').collection('bookings')
+    const userCollection = client.db('doctors_portal').collection('users')
+
+
+    //users
+    app.get('/user',verifyJWT,async(req,res)=>{
+        const users = await userCollection.find().toArray();
+        res.send(users);
+
+    })
+
+    //for user
+    app.put('/user/:email', async(req,res)=>{
+        const email = req.params.email;
+        const user = req.body;
+        const filter = {email: email};
+        const options = {upsert: true};
+        const updateDoc= {
+            $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+        res.send({result,token});
+    })
 
     //create api for service
     app.get('/service', async(req,res) =>{
@@ -64,14 +105,23 @@ try{
  * app.get('/booking/:id')//get a specific booking
  * app.post('/booking') //add a new booking
  * app.patch('/booking/:id')
+ * app.put('/booking/:id)//upsert => update(if exists) of insert(if dose't exists)
  * app.delete('/booking/:id')
  */
 //for dashboard
-app.get('/booking',async(req,res)=>{
+app.get('/booking',verifyJWT,async(req,res)=>{
     const patient = req.query.patient;
+    
+   const decodedEmail = req.decoded.email;
+   if(patient === decodedEmail){
     const query = {patient: patient};
     const booking = await bookingCollection.find(query).toArray();
-    res.send(booking)
+   return res.send(booking);
+   }
+   else{
+       return res.status(403).send({message: 'forbidden access'});
+   }
+   
 })
 //for Appointment
 app.post('/booking',async(req,res)=>{
